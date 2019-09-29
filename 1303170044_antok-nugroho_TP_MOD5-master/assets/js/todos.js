87ce935705ac2c9358a00c1482ca21582a5b6dc9
kagetokikun <36444599+kagetokikun@users.noreply.github.com>
2019-09-29 21:03:46 +0700

    Add files via upload

---
+++ 1303170044_antok-nugroho_TP_MOD5-master/assets/js/todos.js
@@ -0,0 +1,36 @@
+  $('li').click(function() {
+    if ($(this).css('color') === 'rgb(128, 128, 128)') {
+      $(this).css({
+        color: 'black',
+        textDecoration: 'none'
+      });
+    } else {
+      $(this).css({
+        color: 'gray',
+        textDecoration: 'line-through'
+      });
+    }
+  });
+
+  $('#semangat').append(
+    "<audio controls autoplay hidden> <source src='../css/semangat.mp3' type='audio/mpeg' /></audio>"
+  );
+
+  // lengkapi jquery untuk menlist hasil dari todolist yang di inputkan
+  $('ul').on('click', 'li', function() {
+      $(this).css({
+        color: 'gray',
+        textDecoration: 'line-through'
+      });
+  });
+
+  // buatlah jquery untuk menekan tombol akan menjadikan data sebagai list
+    $('#plus').on('click', function(){
+      var input = $('input').val();
+      var tmbh = ` 
+        <li> <span><i class="fa fa-trash"></i></span> ${input} </li>
+      `;
+		
+      $('#todos').append(tmbh);
+      $('input').val('');
+  })
